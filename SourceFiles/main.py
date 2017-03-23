82b08da6a84683a946746b3559c2f35505f7b9a0
GiltosMx <adriantoscanop@gmail.com>
2017-03-23 12:56:21 -0600

    Starts using the requests library instead of python-instagram

--- SourceFiles/main.py
+++ SourceFiles/main.py
@@ -1,5 +1,5 @@
 from instagram.client import InstagramAPI
-from instagram.bind import InstagramAPIError
+import requests
 
 access_token = '2613384557.4b4891d.0e84980f87b74955bd65b27d3fc21c62'
 client_secret = '6425a96b56e64e2aa4e96cca976b2fea'
@@ -15,9 +15,26 @@ def main():
     api = auth(access_token, client_secret)
 
     #api.media_search(q, count, lat, lng, min_timestamp, max_timestamp)
-    print(api.location('2593354')) #Uses the Eiffel Tower Location ID
+    #print(api.location('2593354')) #Uses the Eiffel Tower Location ID
 
-    #api.location_recent_media(count, max_id, location_id)*
+    data = requests.get('https://api.instagram.com/v1/locations/{}?access_token={}'.format('2593354', access_token))
+    # API response:
+    # {
+    #     u'meta':
+    #     {
+    #         u'code': 200
+    #     },
+    #     u'data':
+    #     {
+    #         u'latitude': 48.858212502088, u'id': u'2593354', u'longitude': 2.2945895631526, u'name': u'Tour Eiffel'
+    #     }
+    # }
+
+    print('Data obtained:')
+    print(data.json())
+
+
+
 
 
 
